b2587f91f77c224ba2b8e4f2dee1987cca1ec141
Nico DG <33153912+Poulpynateur@users.noreply.github.com>
2018-05-28 15:47:18 +0200

    Gestion des sprite (chargement)

--- Lorann/model/src/main/java/model/Sprite.java
+++ Lorann/model/src/main/java/model/Sprite.java
@@ -1,22 +1,33 @@
 package model;
 
+import model.dao.CharToSprite;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Sprite {
 
-
-
-    public Image LoadSprite() {
+    public Image LoadSprite(String name) {
         Image picture;
         try {
-            picture = ImageIO.read(new File("images/lorann_b.png"));
+            picture = ImageIO.read(new File(name));
             return picture;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public void LoadAllSprite() {
+        for (CharToSprite sprite : CharToSprite.values()) {
+            sprite.setImage(LoadSprite("sprite\\"+sprite.getName()+".png"));
+        }
+    }
+
+    public Image getSpriteFromString(String sprite) {
+        return CharToSprite.valueOf(sprite).getImage();
     }
 }
