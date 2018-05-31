b7de59089c732fdbd3058a7f5f2cee5dd43f9014
DixibotV2 <38345414+Antoine67@users.noreply.github.com>
2018-05-31 11:38:01 +0200

    Update AIDeplacement.java
    AI improvement

--- Lorann/controller/src/main/java/controller/AIDeplacement.java
+++ Lorann/controller/src/main/java/controller/AIDeplacement.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,78 +13,85 @@ public abstract class AIDeplacement {
 	
 
 	
-	
+
 public static List<Point> moveAI(IModel model) {
 		
 		List<Point> enemiesPos = model.getEnemiesLocation();
-		//List<Point> movePos ;
+		List<Point> newEnemiesPos = new ArrayList<>();
 		Random rand = new Random();
-		
 
 		for (Point enemyMove : enemiesPos) {
+
+
 
 			//Each random int = a specific movement
 			int direction = rand.nextInt(8) + 1;
 			// minimum = 1 // maximum=8 //
 
+
+
             switch(direction) {
 
 				case 1 : // UP
 				{
-					enemyMove.y=(int) (enemyMove.getY()-1.0);
+					newEnemiesPos.add(new Point(0,1));
 					break;
 				}
 
 				case 2 : // DIAGONAL UP RIGHT
 				{
-					enemyMove.x=(int) (enemyMove.getX()+1.0);
-					enemyMove.y=(int) (enemyMove.getY()-1.0);
+					newEnemiesPos.add(new Point(1,1));
 					break;
 				}
 
 				case 3 : // RIGHT
 				{
-					enemyMove.x=(int) (enemyMove.getX()+1.0);
+					newEnemiesPos.add(new Point(1,0));
 					break;
 				}
 
 				case 4 : // DIAGONAL DOWN RIGHT
 				{
-					enemyMove.x=(int) (enemyMove.getX()+1.0);
-					enemyMove.y=(int) (enemyMove.getY()-1.0);
+					newEnemiesPos.add(new Point(1,-1));
 					break;
 				}
 
 				case 5 : // DOWN
 				{
-					enemyMove.y=(int) (enemyMove.getY()-1.0);
+					newEnemiesPos.add(new Point(0,-1));
 					break;
 				}
 
 				case 6 : // DIAGONAL DOWN LEFT
 				{
-					enemyMove.y=(int) (enemyMove.getY()-1.0);
-					enemyMove.x=(int) (enemyMove.getX()-1.0);
+					newEnemiesPos.add(new Point(-1,-1));
 					break;
 				}
 
 				case 7 : // LEFT
 				{
-					enemyMove.x=(int) (enemyMove.getX()-1.0);
+					newEnemiesPos.add(new Point(-1,0));
 					break;
 				}
 
 				case 8 : // DIAGONAL UP LEFT
 				{
-					enemyMove.x=(int) (enemyMove.getX()-1.0);
-					enemyMove.y=(int) (enemyMove.getY()-1.0);
+					newEnemiesPos.add(new Point(-1,1));
 					break;
 				}
             }
             
 		}
 
-		return enemiesPos;
-
+		return newEnemiesPos;
+	
+	/*
+	int i=0;
+	List<Point> oldEnemiesPos = model.getEnemiesLocation();
+	List<Point> newEnemiesPos = new ArrayList<>();
+	for (Point enemyMove : oldEnemiesPos) {
+		newEnemiesPos.add(new Point(1,0));
+	}
+return newEnemiesPos;*/
 }
 }
