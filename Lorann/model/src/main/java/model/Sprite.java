8c67d1d6594c6c35d2ea106e6f9d5d367583c0ac
DixibotV2 <38345414+Antoine67@users.noreply.github.com>
2018-06-01 16:53:11 +0200

    JavaDoc update

--- Lorann/model/src/main/java/model/Sprite.java
+++ Lorann/model/src/main/java/model/Sprite.java
@@ -8,6 +8,12 @@ import java.io.IOException;
 
 public abstract class Sprite {
 
+    /**
+	 * Create the sprite from a name
+	 *
+	 * @return a a Sprite of type Image
+	 * 
+	 */
     public static Image LoadSprite(String name) {
         Image picture;
         try {
@@ -19,12 +25,20 @@ public abstract class Sprite {
         return null;
     }
 
+    /**
+	 * Load all the sprites 
+	 */    
     public static void LoadAllSprite() {
         for (ElementsList sprite : ElementsList.values()) {
         	sprite.setImage(LoadSprite("model/src/main/java/model/sprite/"+sprite.getName()+".png"));
         }
     }
-
+    /**
+	 * Create the sprite from a string
+	 *
+	 * @return  a a Sprite of type Image
+	 * 
+	 */
     public static Image getSpriteFromString(String sprite) {
         return ElementsList.get(sprite).getImage();
     }
