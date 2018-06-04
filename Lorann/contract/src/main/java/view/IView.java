9655949d6f0fb7218baf7aee3b7945595ab649a1
AntoienM <33087066+AntoienM@users.noreply.github.com>
2018-06-04 15:14:46 +0200

    JD

--- Lorann/contract/src/main/java/view/IView.java
+++ Lorann/contract/src/main/java/view/IView.java
@@ -5,8 +5,8 @@ import java.awt.*;
 /**
  * <h1>The Interface IView.</h1>
  *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
+ * @author Groupe 13
+ * @version 2.0
  */
 public interface IView {
 
