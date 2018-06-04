1a4be85f434be9e605892903cc0e49acc8d1215d
DixibotV2 <38345414+Antoine67@users.noreply.github.com>
2018-06-04 14:26:47 +0200

    JD

--- Lorann/model/src/main/java/model/elements/Mobile/Player.java
+++ Lorann/model/src/main/java/model/elements/Mobile/Player.java
@@ -8,11 +8,31 @@ public class Player extends Mobile{
     private int animationNumber = 0;
 
     /**** CONSTRUCTOR ****/
+    
+    /**
+     * Initiate the player
+     * @param stringStylestringStyle the stringStyle (example : "L1" corresponds to the player)
+     * @param x X coordinate
+     * @param y Y coordinate
+     */
     public Player(String stringStyle, int x, int y) {
         super(stringStyle,Types.PLAYER,x,y);
     }
 
     /**** METHODS ****/
+    
+    /**
+     * Animate an element depends on the coordinates
+     * @param directionX Relative X coordinate
+     * @param directionY Realtive Y coordinate
+     * 
+     * @see model.elements.ElementsList#getCharacter()
+     * - Get the character
+     *  @see model.elements.ElementsList#getImage()
+     * - Get the image
+     * @see model.elements.ElementsList#getDirection()
+     * - Get the direction
+     */
     public void animate (int directionX, int directionY){
 
         if (directionX == 0 && directionY == 0) {
