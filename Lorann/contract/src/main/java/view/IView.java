d8eed3cfea6fa7ea545979d2242e7d84a5e13b1d
FelixDufay <33153867+FelixDufay@users.noreply.github.com>
2018-06-05 15:13:30 +0200

    JD

--- Lorann/contract/src/main/java/view/IView.java
+++ Lorann/contract/src/main/java/view/IView.java
@@ -5,17 +5,27 @@ import java.awt.*;
 /**
  * <h1>The Interface IView.</h1>
  *
- * @author Groupe 13
+ * @author Group 13
  * @version 2.0
  */
+
 public interface IView {
 
+	/** refresh the window */
     void showElements();
+    /** print message on screen */
     void displayMessage(String message);
+    /** get player location */
     Point return_deplacement_player();
+    /** check if player is casting spell */
     boolean return_casting_player();
 
+    /** set score */
     void setScore(int score);
+    /** get score */
     int getScore();
+    /** set the array of image */
     void setImageMap(Image[][] map);
+    /** update (observable */
+	void update();
 }
