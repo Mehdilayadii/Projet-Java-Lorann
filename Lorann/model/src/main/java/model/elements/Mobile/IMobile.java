41864ac4b38055adf4c2cc2c666e77aa4b46bbe4
FelixDufay <33153867+FelixDufay@users.noreply.github.com>
2018-06-05 17:06:41 +0200

    JD

--- Lorann/model/src/main/java/model/elements/Mobile/IMobile.java
+++ Lorann/model/src/main/java/model/elements/Mobile/IMobile.java
@@ -4,7 +4,17 @@ import model.elements.IElements;
 
 import java.awt.*;
 
+/**
+ * <h1>The Class Mobile.</h1>
+ *
+ * @author Group 13
+ * @version 2.0
+ */
+
 public interface IMobile extends IElements {
+	
+	/** get the location */
     Point getLocation();
+    /** set the location */
     void setLocation(int x, int y);
 }
