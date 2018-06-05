97daf2bc5f71c814c944c34c280f4f72a716da9e
AntoienM <33087066+AntoienM@users.noreply.github.com>
2018-06-05 15:33:49 +0200

    Test

--- Lorann/view/src/test/java/view/CustomJFrameTest.java
+++ Lorann/view/src/test/java/view/CustomJFrameTest.java
@@ -2,8 +2,10 @@ package view;
 
 import static org.junit.Assert.*;
 
-import java.awt.Point;
+import java.awt.AWTException;
+import java.awt.Robot;
 import java.awt.event.KeyEvent;
+
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,10 +14,17 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CustomJFrameTest {
-	private Point dp1;
+	CustomJFrame jframe;
+	static Robot r = null;
+
+	
+    //jframe.getKeyListeners()[0].keyPressed(key);
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() throws Exception, AWTException {
+		
+		
+        
 	}
 
 	@AfterClass
@@ -24,7 +33,13 @@ public class CustomJFrameTest {
 
 	@Before
 	public void setUp() throws Exception {
-	dp1 = new Point(0,0);
+		try {
+			r = new Robot();
+			jframe = new CustomJFrame();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@After
@@ -38,25 +53,33 @@ public class CustomJFrameTest {
 
 	@Test
 	public void testReturn_casting_player() {
-		fail("Not yet implemented");
+		int expected = 1;
+		int value;
+		jframe.casting_spell = true;
+		if(jframe.return_casting_player()) {
+			value = 1;
+		} else { value =0;}
+		assertEquals(expected,value);
 	}
 
 	
+	
+	
+	
 	@Test
 	public void testKeyPressed() {
-		fail("Not yet implemented");
+		final int expected = 0;
+		r.keyPress(KeyEvent.VK_UP);
+		int vk=jframe.deplacement_player.x;
+		assertEquals(expected, vk);
 	}
 
 	@Test
 	public void testKeyReleased() {
 		final int expected = 0;
-		final int vk=CustomJFrame.keyReleased(KeyEvent.VK_UP).deplacement_player.y;
+		r.keyRelease(KeyEvent.VK_UP);
+		final int vk=jframe.deplacement_player.y;
 		assertEquals(expected, vk);
-	}
-
-	@Test
-	public void testKeyTyped() {
-		fail("Not yet implemented");
 	}
 
 }
