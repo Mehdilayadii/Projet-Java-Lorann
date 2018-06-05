253ad775a65ffe59ee5d38a270f7b8f0c85ba6e6
FelixDufay <33153867+FelixDufay@users.noreply.github.com>
2018-06-05 17:05:03 +0200

    JD

--- Lorann/model/src/main/java/model/elements/IElements.java
+++ Lorann/model/src/main/java/model/elements/IElements.java
@@ -4,8 +4,19 @@ import model.Types;
 
 import java.awt.*;
 
+/**
+ * <h1>The Class Elements.</h1>
+ *
+ * @author Group 13
+ * @version 2.0
+ */
+
 public interface IElements {
+	
+	/** get style */
     String getStringStyle();
+    /** get the sprite */
     Image getSprite();
+    /** get the type */
     Types getType();
 }
